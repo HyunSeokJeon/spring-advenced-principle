64b37965a38cb01f9b3384f7b5b600bf5b0bc978
jeonhyunseok <sjunhs@gmail.com>
2025-01-01 10:39:44 +0900

    interface 기반 proxy 적용 예제

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -4,6 +4,7 @@ import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class LogTraceConfig {
@@ -12,4 +13,11 @@ public class LogTraceConfig {
     public LogTrace logTrace() {
         return new ThreadLocalLogTrace();
     }
+
+
+    @Bean
+    public void test() {
+        new BCryptPasswordEncoder();
+    }
 }
+
